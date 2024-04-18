f7d70060cec576b38795a5d4874eb9c8b3d6b2cb
ncb6206 <ncb6206@gmail.com>
2024-04-18 18:34:53 +0900

    Fix : 빌드 시 발생했던 타입 오류 수정

--- app/(beforeLogin)/layout.tsx
+++ app/(beforeLogin)/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
 
-type Props = { children: ReactNode; modal: ReactNode };
+type Props = { children: ReactNode };
 
 const Layout = ({ children }: Props) => {
   return (
