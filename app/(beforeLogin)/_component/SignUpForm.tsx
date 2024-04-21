d4a48276fadd348dfd2e433166617e6039960c79
ncb6206 <ncb6206@gmail.com>
2024-04-22 02:54:38 +0900

    Refactor : 로그인, 회원가입 폼 리팩토링

--- app/(beforeLogin)/_component/SignUpForm.tsx
+++ app/(beforeLogin)/_component/SignUpForm.tsx
@@ -3,6 +3,8 @@
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { doc, setDoc } from 'firebase/firestore';
+import { useRouter } from 'next/navigation';
+import toast from 'react-hot-toast';
 
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
@@ -25,6 +27,7 @@ interface SignUpInputs {
 }
 
 const SignUpForm = () => {
+  const router = useRouter();
   const {
     register,
     handleSubmit,
@@ -34,7 +37,11 @@ const SignUpForm = () => {
 
   const onSubmit: SubmitHandler<SignUpInputs> = async data => {
     try {
-      await createUserWithEmailAndPassword(auth, data.email, data.password);
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password,
+      );
       const imageUrl = await handleUpload(data.profileImage[0]);
       const collectionUser = doc(db, 'User', auth.currentUser?.uid as string);
       await setDoc(collectionUser, {
@@ -47,8 +54,14 @@ const SignUpForm = () => {
         createdAt: dayjsNow(),
         updatedAt: dayjsNow(),
       });
+
+      if (userCredential.user) {
+        toast.success('회원가입 완료');
+        router.replace('/home');
+      }
     } catch (error) {
-      console.log(error);
+      console.log((error as Error).message);
+      toast.error('회원가입 실패');
     }
   };
 
@@ -58,79 +71,96 @@ const SignUpForm = () => {
     <>
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="max-w-sm space-y-8 overflow-x-auto p-2"
+        className="max-h-dvh w-full space-y-6 overflow-x-auto p-6 sm:p-20"
       >
-        <Input
-          {...register('name', { required: '이름은 필수 입력 항목입니다.' })}
-          id="name"
-          placeholder="이름"
-          type="text"
-          name="name"
-          className="border-0 bg-[#f5f5f5]"
-        />
-        {errors.name && <p className="text-red-600">{errors.name?.message}</p>}
+        <p className="text-center text-2xl font-bold">회원가입</p>
+        <div>
+          <Input
+            {...register('name', { required: '이름은 필수 입력 항목입니다.' })}
+            id="name"
+            placeholder="이름"
+            type="text"
+            name="name"
+            className="border-0 bg-[#f5f5f5]"
+          />
+          {errors.name && (
+            <p className="mt-2 text-red-600">{errors.name?.message}</p>
+          )}
+        </div>
 
-        <Input
-          {...register('nickname', {
-            required: '닉네임은 필수 입력 항목입니다.',
-          })}
-          id="nickname"
-          placeholder="닉네임"
-          type="text"
-          name="nickname"
-          className="border-0 bg-[#f5f5f5]"
-        />
-        {errors.nickname && (
-          <p className="text-red-600">{errors.nickname?.message}</p>
-        )}
+        <div>
+          <Input
+            {...register('nickname', {
+              required: '닉네임은 필수 입력 항목입니다.',
+            })}
+            id="nickname"
+            placeholder="닉네임"
+            type="text"
+            name="nickname"
+            className="border-0 bg-[#f5f5f5]"
+          />
+          {errors.nickname && (
+            <p className="mt-2 text-red-600">{errors.nickname?.message}</p>
+          )}
+        </div>
 
-        <Input
-          {...register('email', { required: '이메일은 필수 입력 항목입니다.' })}
-          id="email"
-          placeholder="이메일 주소"
-          type="email"
-          name="email"
-          className="border-0 bg-[#f5f5f5]"
-        />
-        {errors.email && (
-          <p className="text-red-600">{errors.email?.message}</p>
-        )}
+        <div>
+          <Input
+            {...register('email', {
+              required: '이메일은 필수 입력 항목입니다.',
+            })}
+            id="email"
+            placeholder="이메일 주소"
+            type="email"
+            name="email"
+            className="border-0 bg-[#f5f5f5]"
+          />
+          {errors.email && (
+            <p className="mt-2 text-red-600">{errors.email?.message}</p>
+          )}
+        </div>
 
-        <Input
-          {...register('password', {
-            required: '비밀번호는 필수 입력 항목입니다.',
-            pattern: {
-              value:
-                /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-              message:
-                '비밀번호는 최소 8자 이상, 영어 대문자, 소문자, 숫자, 특수문자 중 3종류 문자 조합으로 구성해야 합니다.',
-            },
-          })}
-          id="password"
-          placeholder="비밀번호"
-          type="password"
-          name="password"
-          className="border-0 bg-[#f5f5f5]"
-        />
-        {errors.password && (
-          <p className="text-red-600">{errors.password?.message}</p>
-        )}
+        <div>
+          <Input
+            {...register('password', {
+              required: '비밀번호는 필수 입력 항목입니다.',
+              pattern: {
+                value:
+                  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+                message:
+                  '비밀번호는 최소 8자 이상, 영어 대문자, 소문자, 숫자, 특수문자 중 3종류 문자 조합으로 구성해야 합니다.',
+              },
+            })}
+            id="password"
+            placeholder="비밀번호"
+            type="password"
+            name="password"
+            className="border-0 bg-[#f5f5f5]"
+          />
+          {errors.password && (
+            <p className="mt-2 text-red-600">{errors.password?.message}</p>
+          )}
+        </div>
 
-        <Input
-          {...register('confirmPassword', {
-            required: '비밀번호 확인은 필수 입력 항목입니다.',
-            validate: value =>
-              value === password || '비밀번호가 일치하지 않습니다.',
-          })}
-          id="confirmPassword"
-          placeholder="비밀번호 확인"
-          type="password"
-          name="confirmPassword"
-          className="border-0 bg-[#f5f5f5]"
-        />
-        {errors.confirmPassword && (
-          <p className="text-red-600">{errors.confirmPassword?.message}</p>
-        )}
+        <div>
+          <Input
+            {...register('confirmPassword', {
+              required: '비밀번호 확인은 필수 입력 항목입니다.',
+              validate: value =>
+                value === password || '비밀번호가 일치하지 않습니다.',
+            })}
+            id="confirmPassword"
+            placeholder="비밀번호 확인"
+            type="password"
+            name="confirmPassword"
+            className="border-0 bg-[#f5f5f5]"
+          />
+          {errors.confirmPassword && (
+            <p className="mt-2 text-red-600">
+              {errors.confirmPassword?.message}
+            </p>
+          )}
+        </div>
 
         <div className="grid w-full max-w-sm items-center gap-1.5">
           <Label htmlFor="profileImage">프로필 이미지</Label>
@@ -145,20 +175,26 @@ const SignUpForm = () => {
             className="border-0 bg-[#f5f5f5] hover:cursor-pointer"
           />
           {errors.profileImage && (
-            <p className="text-red-600">{errors.profileImage?.message}</p>
+            <p className="mt-2 text-red-600">{errors.profileImage?.message}</p>
           )}
         </div>
 
-        <Textarea
-          {...register('bio', { required: '인사말은 필수 입력 항목입니다.' })}
-          id="bio"
-          placeholder="인삿말을 작성해주세요!"
-          name="bio"
-          className="border-0 bg-[#f5f5f5]"
-        />
-        {errors.bio && <p className="text-red-600">{errors.bio?.message}</p>}
+        <div>
+          <Textarea
+            {...register('bio', { required: '인사말은 필수 입력 항목입니다.' })}
+            id="bio"
+            placeholder="인삿말을 작성해주세요!"
+            name="bio"
+            className="border-0 bg-[#f5f5f5]"
+          />
+          {errors.bio && (
+            <p className="mt-2 text-red-600">{errors.bio?.message}</p>
+          )}
+        </div>
 
-        <Button type="submit">회원가입</Button>
+        <div className="flex justify-end">
+          <Button type="submit">회원가입</Button>
+        </div>
       </form>
     </>
   );
