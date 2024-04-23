ccfb30e014bf156ffa5e3d569c9e3a1014142b11
ncb6206 <ncb6206@gmail.com>
2024-04-23 15:38:27 +0900

    Refactor :  메인으로 가는 버튼 추가

--- app/(beforeLogin)/login/page.tsx
+++ app/(beforeLogin)/login/page.tsx
@@ -1,9 +1,17 @@
-import LoginForm from '@/app/(beforeLogin)/_component/LoginForm';
+import Link from 'next/link';
+import { MdOutlineCancel } from 'react-icons/md';
+
+import LoginForm from '@/app/(BeforeLogin)/login/_component/LoginForm';
 
 const LoginPage = () => {
   return (
-    <div className="my-auto flex w-full flex-col items-center justify-center">
-      <LoginForm />
+    <div className="relative my-auto flex h-dvh w-full flex-col items-center justify-center">
+      <div className="absolute left-0 top-0 m-5">
+        <Link className="hover:cursor-pointer" href="/">
+          <MdOutlineCancel size={28} />
+        </Link>
+        <LoginForm />
+      </div>
     </div>
   );
 };
--- app/(beforeLogin)/signup/page.tsx
+++ app/(beforeLogin)/signup/page.tsx
@@ -1,8 +1,16 @@
-import SignUpForm from '@/app/(beforeLogin)/_component/SignUpForm';
+import Link from 'next/link';
+import { MdOutlineCancel } from 'react-icons/md';
+
+import SignUpForm from '@/app/(BeforeLogin)/signup/_component/SignUpForm';
 
 const SignUpPage = () => {
   return (
-    <div className="my-auto flex w-full flex-col items-center justify-center">
+    <div className="relative my-auto flex h-dvh w-full flex-col items-center justify-center">
+      <div className="absolute left-0 top-0 m-5">
+        <Link className="hover:cursor-pointer" href="/">
+          <MdOutlineCancel size={28} />
+        </Link>
+      </div>
       <SignUpForm />
     </div>
   );
