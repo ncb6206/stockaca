b3032ca69d7bc0f3b99bcb4e216eef7ca5aa22ee
ncb6206 <ncb6206@gmail.com>
2024-04-24 13:43:05 +0900

    Feat : user 프로필 데이터 가져오기

--- app/(afterLogin)/users/[userId]/page.tsx
+++ app/(afterLogin)/users/[userId]/page.tsx
@@ -1,13 +1,32 @@
 import LogoutButton from '@/app/(afterLogin)/users/[userId]/_component/LogoutButton';
+import { getUser } from '@/app/(afterLogin)/users/[userId]/_lib/getUser';
+import Image from 'next/image';
 
 interface Props {
   params: { userId: string };
 }
 
-const UserPage = ({ params }: Props) => {
+const UserPage = async ({ params }: Props) => {
+  const userId = decodeURIComponent(params.userId);
+  const userData = await getUser({ email: userId });
+  console.log(userData);
+
+  if (!userData) {
+    return <div>이런! 사용자를 찾을 수 없습니다.</div>;
+  }
+
   return (
     <div className="flex flex-col">
-      {params.userId}님의 페이지입니다.
+      <p>{userData?.name}</p>
+      <p>{userData?.nickname}</p>
+      <p>{userData?.bio}</p>
+      <p>{userData?.email}님의 페이지입니다.</p>
+      <Image
+        src={userData?.profileImage}
+        alt="프로필 사진"
+        width={300}
+        height={300}
+      />
       <LogoutButton />
     </div>
   );
