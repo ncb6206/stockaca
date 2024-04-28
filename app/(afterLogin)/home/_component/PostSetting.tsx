5e92653c0aa2111de7ce72707347d131b230468c
ncb6206 <ncb6206@gmail.com>
2024-04-29 02:34:22 +0900

    Feat : 게시글 삭제 구현

---
+++ app/(afterLogin)/home/_component/PostSetting.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { MouseEvent } from 'react';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
+import { BsThreeDots } from 'react-icons/bs';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { IPostId } from '@/app/types/post';
+import { deletePost } from '../_lib/deletePost';
+
+const PostSetting = ({ postId }: IPostId) => {
+  const queryClient = useQueryClient();
+  const router = useRouter();
+
+  const deleteFeed = useMutation({
+    mutationFn: () => deletePost({ postId }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['posts'] });
+      toast.success('삭제되었습니다!');
+    },
+    onError: error => {
+      console.error('Error deleting post:', error);
+      toast.error('게시글 삭제 실패');
+    },
+  });
+
+  const onEditPost = (event: MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation();
+    router.push(`/post`);
+  };
+
+  const onDeletePost = (event: MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation();
+    deleteFeed.mutate();
+  };
+
+  return (
+    <div className="relative">
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <BsThreeDots />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="absolute -right-3">
+          <DropdownMenuItem
+            className="hover:cursor-pointer"
+            onClick={onEditPost}
+          >
+            수정
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className="hover:cursor-pointer"
+            onClick={onDeletePost}
+          >
+            삭제
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+export default PostSetting;
