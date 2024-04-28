b7b8c05be55ea35ff07dca78ce030891e7b81f58
ncb6206 <ncb6206@gmail.com>
2024-04-29 02:48:21 +0900

    Feat : 댓글 표시 컴포넌트 추가

---
+++ app/(afterLogin)/home/_component/CommentCount.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { FaRegComment } from 'react-icons/fa';
+
+interface CommentCountProps {
+  commentCount: number;
+}
+
+const CommentCount = ({ commentCount }: CommentCountProps) => {
+  return (
+    <div className="flex">
+      <div className="flex items-center gap-1 text-gray-500">
+        <FaRegComment className="text-" />
+        <p>{commentCount}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CommentCount;
