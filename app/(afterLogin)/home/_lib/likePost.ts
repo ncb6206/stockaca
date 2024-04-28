c9d9574ea03c00f0834845d87a86c846cf9832e1
Natest <62326659+ncb6206@users.noreply.github.com>
2024-04-29 02:56:17 +0900

    좋아요 기능 구현 (#29)
    * Chore : textarea autosize 라이브러리 설치
    
    * Refactor : react query 옵션 수정
    
    * Feat : 로딩중일 때 버튼 분기 처리
    
    * Refactor : react query로 유저 프로필 데이터 가져오기
    
    * Feat : 게시글 작성 페이지 구현
    
    * Feat : dialog 공통 컴포넌트 추가
    
    * Refactor : 이미지 저장 경로 수정
    
    * Refactor : 게시글 작성시 네비게이션 바 숨김
    
    * Chore : 라이브러리 설치 및 배포시 콘솔 로그 안뜨게 설정
    
    * Chore : eslint 설정 추가
    
    * feat : firebase 콜렉션 연결 추가
    
    * Feat : 전체 게시글 조회 기능 추가
    
    * Feat : 게시글 상세 페이지 구현
    
    * Feat : 게시글 삭제 구현
    
    * Style : Dropdown 관련 스타일 수정
    
    * Feat : 게시글 수정 레이아웃 설정
    
    * Feat : 유저타입과 좋아요 타입 설정
    
    * Refactor : 특정 경로에 따라 뒤로가기 표시 설정
    
    * Feat : createdAt 시간 표시 방법 변경
    
    * Feat : 메인 피드 로딩창 설정
    
    * Refactor : 유저타입명 변경으로 인한 타입명 변경
    
    * Refactor : 게시글 작성 로직 분리
    
    * Feat : 댓글 표시 컴포넌트 추가
    
    * Refactor : 로그인 중이 아니라면 로그아웃 버튼 hidden
    
    * Feat : 좋아요 컴포넌트 추가
    
    * Feat : 좋아요 불러오기 기능 추가
    
    * Feat : 좋아요 / 좋아요 취소 기능 추가

---
+++ app/(afterLogin)/home/_lib/likePost.ts
@@ -0,0 +1,27 @@
+import { addDoc, doc, increment, updateDoc } from 'firebase/firestore';
+
+import { LIKE_COLLECTION, db } from '@/app/firebase';
+import { ILikeData } from '@/app/types/like';
+
+export const likePost = async ({ postId, userId }: ILikeData) => {
+  try {
+    const postRef = doc(db, 'Feed', postId);
+    const likeRef = LIKE_COLLECTION;
+    const likeData = {
+      createdAt: Date.now(),
+      feedId: postId,
+      userId,
+    };
+    await addDoc(likeRef, likeData);
+
+    await updateDoc(postRef, {
+      likeCount: increment(1),
+    });
+
+    console.log('좋아요');
+    return true;
+  } catch (error) {
+    console.log('Error like post:', error);
+    throw new Error('Failed to like post');
+  }
+};
