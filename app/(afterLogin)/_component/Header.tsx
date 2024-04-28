0ccb2f34936e3347fefaf9d9d528f55364e86fac
ncb6206 <ncb6206@gmail.com>
2024-04-29 02:43:17 +0900

    Refactor : 특정 경로에 따라 뒤로가기 표시 설정

--- app/(afterLogin)/_component/Header.tsx
+++ app/(afterLogin)/_component/Header.tsx
@@ -1,18 +1,29 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { IoArrowBack } from 'react-icons/io5';
 
 const Header = () => {
   const router = useRouter();
+  const pathname = usePathname();
+
+  const showBackButtonRoutes = ['/post', '/home/'];
+  const shouldShowBackButton = showBackButtonRoutes.some(path =>
+    pathname.startsWith(path),
+  );
 
   const goBack = () => {
     router.back();
   };
 
   return (
-    <header className="fixed left-0 top-0 z-10 flex h-16 w-full items-center bg-background px-4">
-      <IoArrowBack className="h-8 w-8 hover:cursor-pointer" onClick={goBack} />
+    <header className="fixed z-10 flex h-16 w-full max-w-screen-sm items-center bg-background px-4">
+      {shouldShowBackButton && (
+        <IoArrowBack
+          className="h-8 w-8 hover:cursor-pointer"
+          onClick={goBack}
+        />
+      )}
     </header>
   );
 };
