1489b85ed753212fcc90ccdb6685eb73bd932f08
ncb6206 <ncb6206@gmail.com>
2024-05-04 13:49:15 +0900

    Refactor : 특정 게시물 관련 커스텀훅 분리

--- app/(afterLogin)/[userId]/post/[postId]/_component/SinglePost.tsx
+++ app/(afterLogin)/[userId]/post/[postId]/_component/SinglePost.tsx
@@ -1,28 +1,15 @@
 'use client';
 
-import { useQuery } from '@tanstack/react-query';
-
-import { IPostData } from '@/app/types/post';
-import { getPost } from '@/app/(afterLogin)/[userId]/post/[postId]/_lib/getPost';
 import PostCard from '@/app/(afterLogin)/_component/PostCard';
+import useGetSinglePost from '@/app/(afterLogin)/[userId]/post/[postId]/_hook/useGetSinglePost';
 
-interface SinglePostProps {
+export interface SinglePostProps {
   userId: string;
   postId: string;
 }
 
 const SinglePost = ({ userId, postId }: SinglePostProps) => {
-  const { data: postData, error } = useQuery<
-    IPostData | null,
-    Object,
-    IPostData,
-    [_1: string, _2: string, _3: string]
-  >({
-    queryKey: [userId, 'post', postId],
-    queryFn: getPost,
-    staleTime: 60 * 1000,
-    gcTime: 300 * 1000,
-  });
+  const { data: postData, error } = useGetSinglePost({ userId, postId });
 
   if (!postData || error) {
     return <div>이런! 페이지를 찾을 수 없습니다.</div>;
--- app/(afterLogin)/home/_component/PostSetting.tsx
+++ app/(afterLogin)/home/_component/PostSetting.tsx
@@ -1,10 +1,6 @@
 'use client';
 
-import { MouseEvent } from 'react';
-import toast from 'react-hot-toast';
-import { useRouter } from 'next/navigation';
 import { BsThreeDots } from 'react-icons/bs';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import {
   DropdownMenu,
@@ -13,8 +9,7 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { IPostSetting } from '@/app/types/post';
-import { deletePost } from '@/app/(afterLogin)/home/_lib/deletePost';
-import { usePostStore } from '@/app/store/usePost';
+import usePostSetting from '../_hook/usePostSetting';
 
 const PostSetting = ({
   userId,
@@ -22,40 +17,11 @@ const PostSetting = ({
   parentPostId,
   isCommentOwner,
 }: IPostSetting) => {
-  const queryClient = useQueryClient();
-  const { setMode, setParentPostId } = usePostStore();
-  const router = useRouter();
-
-  const deleteFeed = useMutation({
-    mutationFn: () => deletePost({ postId, parentPostId }),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['posts'] });
-      if (parentPostId) {
-        queryClient.invalidateQueries({
-          queryKey: ['post', parentPostId, 'comments'],
-        });
-      }
-      toast.success('삭제되었습니다!');
-    },
-    onError: error => {
-      console.error('Error deleting post:', error);
-      toast.error('게시글 삭제 실패');
-    },
+  const { onEditPost, onDeletePost } = usePostSetting({
+    userId,
+    postId,
+    parentPostId,
   });
-
-  const onEditPost = (event: MouseEvent<HTMLDivElement>) => {
-    event.stopPropagation();
-    if (parentPostId) {
-      setMode('comment');
-      setParentPostId(parentPostId);
-    }
-    router.push(`/${userId}/post/${postId}/edit`);
-  };
-
-  const onDeletePost = (event: MouseEvent<HTMLDivElement>) => {
-    event.stopPropagation();
-    deleteFeed.mutate();
-  };
 
   return (
     <div className="relative">
