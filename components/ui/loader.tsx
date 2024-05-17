6aa908fadd68bd5974607d04898353ff261fa1d6
ncb6206 <ncb6206@gmail.com>
2024-05-17 19:28:12 +0900

    Refactor : loader 관련 수정

--- components/ui/loader.tsx
+++ components/ui/loader.tsx
@@ -2,7 +2,7 @@ import RotateLoader from 'react-spinners/RotateLoader';
 
 const Loader = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="my-4 flex flex-col items-center justify-center">
       <RotateLoader color="#9333ea" size={15} />
     </div>
   );
