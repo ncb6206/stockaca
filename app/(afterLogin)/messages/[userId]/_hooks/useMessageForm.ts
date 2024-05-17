e5b4b43656763aec42c6f2fa7b84ce72464eeb24
ncb6206 <ncb6206@gmail.com>
2024-05-18 03:05:54 +0900

    Feat : 메세지 전송 커스텀 훅 추가

--- app/(afterLogin)/messages/[userId]/_hooks/useMessageForm.ts
+++ app/(afterLogin)/messages/[userId]/_hooks/useMessageForm.ts
@@ -1,44 +1,24 @@
-import toast from 'react-hot-toast';
 import { KeyboardEvent } from 'react';
-import { useMutation } from '@tanstack/react-query';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
 import { IMessage } from '@/app/_types/message';
-import useOnAuth from '@/app/_hooks/useOnAuth';
-import { sendMessage } from '@/app/(afterLogin)/messages/[userId]/_services/sendMessage';
+import useSendMessage from '@/app/(afterLogin)/messages/[userId]/_hooks/useSendMessage';
 
 interface IUseMessageForm {
   receiverId: string;
 }
 
 const useMessageForm = ({ receiverId }: IUseMessageForm) => {
-  const { user } = useOnAuth();
   const {
     register,
     handleSubmit,
     reset,
     formState: { isSubmitting },
   } = useForm<IMessage>({ mode: 'onSubmit' });
+  const { sendToMessage } = useSendMessage({ receiverId, reset });
 
-  const sendToMessage = useMutation({
-    mutationFn: (data: IMessage) =>
-      sendMessage({
-        senderId: user?.displayName as string,
-        receiverId,
-        content: data.content,
-      }),
-    onSuccess: () => {
-      reset();
-      toast.success('메시지 보내기 성공');
-    },
-    onError: error => {
-      console.error('Error sending message:', error);
-      toast.error('메시지 보내기 실패');
-    },
-  });
-
-  const onSubmit: SubmitHandler<IMessage> = data => {
-    sendToMessage.mutate(data);
+  const onSubmit: SubmitHandler<IMessage> = async data => {
+    await sendToMessage.mutateAsync(data);
   };
 
   const handleKeyDown = (e: KeyboardEvent<HTMLTextAreaElement>) => {
